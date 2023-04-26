c0238886fb48d32cd1d36cbea3c5d68d2f41d731
Lesley <lci1@williams.edu>
2023-04-26 16:06:26 -0400

    progress

--- WordGen.java
+++ WordGen.java
@@ -1,5 +1,6 @@
 // Students, please implement this class
-
+// Reads the input text, builds the table, and prints out a randomly-generated
+// string based on the character sequence probabilities from the input text.
 public class WordGen {
 
 }
