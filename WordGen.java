342a9296196c328b32250bb87a7c5f9f1b3020d3
Lesley <lci1@williams.edu>
2023-04-30 17:39:58 -0400

    assets comments javadocs

--- WordGen.java
+++ WordGen.java
@@ -2,54 +2,69 @@
 // Reads the input text, builds the table, and prints out a randomly-generated
 // string based on the character sequence probabilities from the input text.
 import structure5.*;
-import java.util.Random;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 public class WordGen {
-    // Scanner in = new Scanner(System.in);
-    // StringBuffer textBuffer = new StringBuffer();
-    // while (in.hasNextLine()) {
-    //     String line = in.nextLine();
-    //     textBuffer.append(line);
-    //     textBuffer.append("\n");
-    // }
-    // String text = textBuffer.toString();
-    // 'text' now contains the full contents of the input.
-    
-    
-    // Table table = new Table();
-    // int k = 2;
-    // String example = "The theatre is their thing";
-    // //int length = example.length();
-    // for (int i = 0; i < example.length() - k; i++) {
-    //     String tableKey = example.substring(i, k + i);
-    //     char freqTableKey = example.charAt(k + i);
-    //     table.add(tableKey, freqTableKey);
-    // }
-    // //System.out.println(table.toString());
 
-    // String seed = example.substring(0, k);
-    // for (int j = 0; j < 10; j++) {
-    //     char f = table.choose(seed.substring(j, j + k));
-    //     seed = seed + f;
-    // }
-
-    public static void main(String[] args) {
+    public static String loadFile() {
+        Scanner in = new Scanner(System.in);
+        StringBuffer textBuffer = new StringBuffer();
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            textBuffer.append(line);
+            textBuffer.append("\n");
+        }
+        String text = textBuffer.toString();
+        return text;
+    }
+    
+    public static String generateText(int k, String text) {
         Table table = new Table();
-        int k = 2;
-        String example = "The theatre is their thing";
         //int length = example.length();
-        for (int i = 0; i < example.length() - k; i++) {
-            String tableKey = example.substring(i, k + i);
-            char freqTableKey = example.charAt(k + i);
+        for (int i = 0; i < text.length(); i++) {
+            String tableKey = "";
+            for (int j = i; j < i + k; j++) {
+                tableKey += text.charAt(j % text.length());
+            }
+            char freqTableKey = text.charAt((k + i) % text.length());
             table.add(tableKey, freqTableKey);
         }
         //System.out.println(table.toString());
-
-        String seed = example.substring(0, k);
-        for (int j = 0; j < 50; j++) {
+        String seed = text.substring(0, k);
+        for (int j = 0; j < 500; j++) {
             char f = table.choose(seed.substring(j, j + k));
             seed = seed + f;
         }
-        System.out.println(seed);
+        return seed;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 1) { // if an argument is passed into the terminal...
+            int k = Integer.parseInt(args[0]);
+            Assert.condition(k > 0, "k must be greater than 0");
+            String text = loadFile();
+            System.out.println(generateText(k, text));
+        } else { // if no argument is passed into the terminal...
+            System.out.println("Usage: java WordGen <k> < <input.txt>");
+            System.exit(1);
+        }
     }
 }
+
+
+// Table table = new Table();
+// int k = 2;
+// String example = "The theatre is their thing";
+// //int length = example.length();
+// for (int i = 0; i < example.length() - k; i++) {
+//     String tableKey = example.substring(i, k + i);
+//     char freqTableKey = example.charAt(k + i);
+//     table.add(tableKey, freqTableKey);
+// }
+// //System.out.println(table.toString());
+// String seed = example.substring(0, k);
+// for (int j = 0; j < 50; j++) {
+//     char f = table.choose(seed.substring(j, j + k));
+//     seed = seed + f;
+// }
+// System.out.println(seed);
